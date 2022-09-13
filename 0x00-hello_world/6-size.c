1930dab5394802ca2276abc3feb79dd29fd25970
Sirmuell <adewumisamuel@gmail.com>
2022-09-13 04:05:21 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 
 /**
  * main - Entry point
+ * 
  * Return: Always 0 (success)
  */
 int main(void)
@@ -17,5 +18,5 @@ int main(void)
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f))
-		return (0);
+	return (0);
 }
