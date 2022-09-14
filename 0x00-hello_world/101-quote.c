0fe13d2aea80aeb68a4ef66ae0fff53a4b6265fc
Sirmuell <adewumisamuel@gmail.com>
2022-09-14 14:58:40 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	write(2,
-			"and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+			"and that piece of art is useful\" - Dora Kopar, 2015-10-19\n");
 	return (1);
 }
