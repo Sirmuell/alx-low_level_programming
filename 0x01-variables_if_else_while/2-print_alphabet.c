f0fda2dd6cd3bc989afce3bb542926ff7edfed2d
Sirmuell <adewumisamuel@gmail.com>
2023-02-17 15:11:58 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,13 @@
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++);
+char ch = 'a';
+while (ch <= 'z')
 {
-	putchar(ch);
+putchar(ch);
+++ch;
 }
-	putchar('\n');
+putchar('\n');
+
 return (0);
 }
