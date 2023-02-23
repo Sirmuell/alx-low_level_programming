6becfd9494a2b15953bc516afb456499b6e5b335
Sirmuell <adewumisamuel@gmail.com>
2023-02-23 12:32:43 -0800

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 
 /**
  * _isupper - function to check if
- * 	character is uppercase
+ * character is uppercase
  *
  * @c: checks input of function
  *
  * Return: returns 1 if `c` is uppercase
- * 	otherwise always 0 (Success)
+ * otherwise always 0 (Success)
  */
 int _isupper(int c)
 {
