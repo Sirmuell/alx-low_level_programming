e7cba767f21c5d4720e44616d0202ff27d08a268
Sirmuell <adewumisamuel@gmail.com>
2023-03-13 09:55:29 -0700

    up to date

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,7 +29,7 @@ s1_length++;
 while (*(s2 + s2_length) != '\0')
 s2_length++;
 
-alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
+alloc_mem = malloc(1 + (s1_length *sizeof(*s1)) + (s2_length *sizeof(*s2)));
 if (alloc_mem == NULL)
 return ('\0');
 
